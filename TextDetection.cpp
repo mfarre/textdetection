c0f3b68bc0d9d16cfb32a92b875079a00a48fcef
Miquel Angel Farre <miquel.farre@gmail.com>
2011-04-03 18:01:10 +0200

    added license

--- TextDetection.cpp
+++ TextDetection.cpp
@@ -1,3 +1,20 @@
+/*
+*Copyright 2011 Miquel Angel Farre Guiu
+*
+*This file is part of my personal textDetector implementation based in the Microsoft Research Publication 
+*"Detecting Text in Natural Scenes with Stroke Width Transform".
+*
+*textDetector is free software: you can redistribute it and/or modify it under the terms of the GNU General
+*Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+*any later *version.
+*
+*textDetector is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+*warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
+*/
+
+
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/connected_components.hpp>
